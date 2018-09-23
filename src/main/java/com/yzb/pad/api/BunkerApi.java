29f0e3488aa26ce887f4f764d2d78de569aeb068
yzbbanban <yzbbanban@live.com>
2018-09-23 14:58:57 +0800

    添加数据查询

--- src/main/java/com/yzb/pad/api/BunkerApi.java
+++ src/main/java/com/yzb/pad/api/BunkerApi.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 /**
  * Created by brander on 2018/9/23
